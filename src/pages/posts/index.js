86b7f4611a5d8c7c2cc4603743071a0318118036
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-03-09 16:30:51 -0300

    Create Posts View.

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import { ColumnDirective, ColumnsDirective, Filter, GridComponent, Group, Inject, Page, Sort } from '@syncfusion/ej2-react-grids';
 import './index.scss';
+import { posts } from './data.js';
 
 const PostsView = () => {
     return (
         <Layout>
-            <h3>Posts Here</h3>;
+            <div className="grid-container">
+                <h1>Tabla de Posteos</h1>
+                <GridComponent dataSource={posts} allowPaging={true} pageSettings={{ pageSize: 6 }} allowFiltering={true} allowGrouping={true} allowSorting={true}>
+                    <ColumnsDirective>
+                        <ColumnDirective field='id_post' headerText='Post ID' width={100} />
+                        <ColumnDirective field='thumbnail' headerText='Thumbnail' />
+                        <ColumnDirective field='title' headerText='Title' />
+                        <ColumnDirective field='text' headerText='Text' />
+                        <ColumnDirective field='authors' headerText='Authors' />
+                        <ColumnDirective field='date' headerText='Date' width={180} />
+                    </ColumnsDirective>
+                    <Inject services={[Page, Filter, Group, Sort]} />
+                </GridComponent>
+            </div>
         </Layout>
     )
 }
